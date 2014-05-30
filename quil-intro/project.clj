1854a5b01745e35b7e71cc498ac4becd7678b9c2
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-05-30 10:39:00 +0100

    Update project.clj

--- quil-intro/project.clj
+++ quil-intro/project.clj
@@ -1,7 +1,5 @@
 (defproject quil-intro "0.1.0-SNAPSHOT"
-  :description "Simple Quil app"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Quil Into"
+  :url "http://nbeloglazov.com/2014/05/29/quil-intro.html"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [quil "2.0.0"]])
