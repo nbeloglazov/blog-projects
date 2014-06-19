de06b3d37d5bc4b94082888158ef6b01dbfbdbed
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-06-19 22:27:31 +0100

    Move to clojure 2.1.0.

--- quil-intro/src/quil_intro.clj
+++ quil-intro/src/quil_intro.clj
@@ -1,31 +1,31 @@
 (ns quil-intro
-  (:require [quil.core :refer :all]))
+  (:require [quil.core :as q]))
 
 ;;; parametric functions in the order they were shown in the post
 
 (defn spiral [t]
-  [(* t (sin t))
-   (* t (cos t))])
+  [(* t (q/sin t))
+   (* t (q/cos t))])
 
 (defn flower [t]
-  (let [r (* 200 (sin t) (cos t))]
-    [(* r (sin (* t 0.2)))
-     (* r (cos (* t 0.2)))]))
+  (let [r (* 200 (q/sin t) (q/cos t))]
+    [(* r (q/sin (* t 0.2)))
+     (* r (q/cos (* t 0.2)))]))
 
 (defn water-drop [t]
-  (let [r (* 1.5 t (cos t) (sin t))]
-    [(* r (sin t))
-     (* r (tan t))]))
+  (let [r (* 1.5 t (q/cos t) (q/sin t))]
+    [(* r (q/sin t))
+     (* r (q/tan t))]))
 
 (defn leaf [t]
-  (let [r (* 1.5 t (cos t) (sin t))]
-    [(* r (cos t))
-     (* r (tan t))]))
+  (let [r (* 1.5 t (q/cos t) (q/sin t))]
+    [(* r (q/cos t))
+     (* r (q/tan t))]))
 
 (defn f [t]
-  (let [r (* 3 t (cos t) (sin t))]
-    [(* r (cos (* 2 t)))
-     (* r (sin t))]))
+  (let [r (* 3 t (q/cos t) (q/sin t))]
+    [(* r (q/cos (* 2 t)))
+     (* r (q/sin t))]))
 
 ;;; end of parametric functions
 
@@ -34,28 +34,28 @@
   (doseq [two-points (->> (range from to step)
                           (map f)
                           (partition 2 1))]
-    (apply line two-points)))
+    (apply q/line two-points)))
 
 ; this is 'draw' from first half of the post
 ; draws static plot
 (defn draw-static []
-  (with-translation [(/ (width) 2) (/ (height) 2)]
+  (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
     (draw-plot f 0 100 0.01)))
 
 (defn setup []
-  (frame-rate 60)
-  (background 255)
-  (color-mode :hsb 10 1 1))
+  (q/frame-rate 60)
+  (q/background 255)
+  (q/color-mode :hsb 10 1 1))
 
 ; this is 'draw' from second half of the post
 (defn draw-animation []
-  (with-translation [(/ (width) 2) (/ (height) 2)]
-    (let [t (/ (frame-count) 10)]
+  (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
+    (let [t (/ (q/frame-count) 10)]
       ; set color for line
-      (stroke (mod t 10) 1 1)
-      (line (flower t) (flower (+ t 0.1))))))
+      (q/stroke (mod t 10) 1 1)
+      (q/line (flower t) (flower (+ t 0.1))))))
 
-(defsketch trigonometry
+(q/defsketch trigonometry
   :size [300 300]
   :setup setup
   :draw draw-animation)
