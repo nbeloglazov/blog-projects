9ab490616a770f006a4703a7f8b2337571ebed9e
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-08-05 22:54:05 +0100

    Update project.clj

--- quil-intro/project.clj
+++ quil-intro/project.clj
@@ -2,4 +2,4 @@
   :description "Quil Into"
   :url "http://nbeloglazov.com/2014/05/29/quil-intro.html"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.2.0"]])
+                 [quil "2.2.1"]])
