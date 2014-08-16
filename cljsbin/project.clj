1366e340ade250f3cd249cbfba8f43afee32fc72
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-08-17 00:01:31 +0100

    Updates.

--- cljsbin/project.clj
+++ cljsbin/project.clj
@@ -1,8 +1,6 @@
 (defproject cljsbin "0.1.0-SNAPSHOT"
   :description "Poor Man's Cljsfiddle"
   :url "http://nbeloglazov.com/2014/08/16/poor-mans-cljsfiddle.html"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [hiccup "1.0.5"]
