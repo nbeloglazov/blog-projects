c49e75f14d4479d4974baa2e56f88d5bcd0fd23b
Nikita Beloglazov <nikelandjelo@gmail.com>
2015-01-31 16:34:30 -0800

    Update project.clj

--- quil-age-of-middleware/project.clj
+++ quil-age-of-middleware/project.clj
@@ -1,5 +1,5 @@
 (defproject quil-age-of-middleware "0.1.0-SNAPSHOT"
   :description "Quil: Age of Middleware"
-  :url "http://nbeloglazov.com/2014/05/29/quil-intro.html"
+  :url "http://nbeloglazov.com/2014/06/22/quil-age-of-middleware.html"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [quil "2.2.5"]])
